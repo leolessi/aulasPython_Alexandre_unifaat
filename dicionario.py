f8ea851be2bfc66948aa3ec881ca24155d01eca5
Leonardo Lessi <162193718+leolessi@users.noreply.github.com>
2025-02-24 20:56:00 -0300

    exercicio aula python

---
+++ dicionario.py
@@ -0,0 +1,5 @@
+produto = {"código": 123, "nome": "Caneta", "preço": 1.50}
+
+print(f"Código: {produto['código']}")
+print(f"Nome: {produto['nome']}")
+print(f"Preço: {produto['preço']}")
---
+++ tupla.py
@@ -0,0 +1,7 @@
+cidade = ("Atibaia", "São Paulo", "Brasil", "12940-000", "160 mil habitantes")
+
+print("Cidade:", cidade[0])
+print("Estado:", cidade[1])
+print("País:", cidade[2])
+print("CEP:", cidade[3])
+print("População:", cidade[4])
